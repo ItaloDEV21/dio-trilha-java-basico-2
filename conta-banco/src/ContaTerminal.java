a0df3f6854396b3bc4a56d505616ea1ed67b9a64
ItaloDEV21 <sampaioitalo21@gmail.com>
2025-03-27 21:52:45 -0300

    desafio simuladno conta báncaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //TODO:conhecer e importar a classe scanner
+
+       //Exibir as mensagens para o nosso usúario 
+
+       // obter pela scanner os valores digitados no terminal 
+
+       // exibir a mensagem da conta criada
+
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+       System.out.println("Por favor, digite o número da Agência !");
+       int numero = scanner.nextInt();
+
+       System.out.println("Informe sua agência: ");
+       String agencia = scanner.next();
+
+       scanner.nextLine();
+
+        System.out.println("Digite seu nome por favor: ");
+        String nome = scanner.nextLine();  
+        
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();  
+
+        //  imprimindo a mesnsagem a ser mostrada para o cliente
+
+        System.out.println("Óla " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
